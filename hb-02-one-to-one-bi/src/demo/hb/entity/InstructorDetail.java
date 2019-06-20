90435e0d533c703a7e7e7139d5809f4c1441ba8c
Subhashis Dhar <sbhdhr@gmail.com>
2019-06-20 20:58:21 +0530

    Added hb-one-to-one-bi

---
+++ hb-02-one-to-one-bi/src/demo/hb/entity/InstructorDetail.java
@@ -0,0 +1,95 @@
+package demo.hb.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+//TODO
+// annotate the class as an entity and map to db table
+// define the fields
+// annotate the fields with db column names
+// create constructors
+// generate getter/setter methods
+// generate tostring() method
+
+@Entity
+@Table(name = "instructor_detail")
+public class InstructorDetail {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "youtube_channel")
+	private String youtubeChannel;
+
+	@Column(name = "hobby")
+	private String hobby;
+
+	// add new field for instructor
+	// add getter/setter
+	// Add @OneToOne annotation
+	
+	//@OneToOne(mappedBy = "instructorDetail", cascade = CascadeType.ALL)
+	//To delete only InstructorDetail change cascade type from CascadeType.ALL 
+	//add all cascade except all
+	
+	@OneToOne(mappedBy = "instructorDetail", 
+			cascade = {CascadeType.DETACH,
+						CascadeType.MERGE,
+						CascadeType.PERSIST,
+						CascadeType.REFRESH})
+	private Instructor instructor;
+
+	public InstructorDetail() {
+	}
+
+	public InstructorDetail(String youtubeChannel, String hobby) {
+		this.youtubeChannel = youtubeChannel;
+		this.hobby = hobby;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getYoutubeChannel() {
+		return youtubeChannel;
+	}
+
+	public void setYoutubeChannel(String youtubeChannel) {
+		this.youtubeChannel = youtubeChannel;
+	}
+
+	public String getHobby() {
+		return hobby;
+	}
+
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
+
+	@Override
+	public String toString() {
+		return "InstructorDetail [id=" + id + ", youtubeChannel=" + youtubeChannel + ", hobby=" + hobby + "]";
+	}
+
+}
